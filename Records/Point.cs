fc9ce3585b182a80cd8f97ff30285f920b475019
Jasper Kent <jasper_kent@hotmail.com>
2021-01-22 08:42:06 +0000

    Add project files.

---
+++ Records/Point.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Records
+{
+    public class PointC
+    {
+        public double X { get; init; }
+        public double Y { get; init; }
+        public double Z { get; init; }
+    }
+
+    public struct PointS
+    {
+        public double X { get; init; }
+        public double Y { get; init; }
+        public double Z { get; init; }
+    }
+
+    // Positional record 
+    // public record PointR(double X, double Y, double Z);
+
+    // Manual record
+    public record PointR
+    {
+        public double X { get; init; }
+        public double Y { get; init; }
+        public double Z { get; init; }
+
+        public override string ToString() => $"({X:N2}, {Y:N2}, {Z:N2})";
+
+        public virtual bool Equals(PointR other)
+        {
+            Console.WriteLine("Bing!");
+
+            return X == other.X && Y == other.Y && Z == other.Z;
+        }
+
+        public override int GetHashCode() => HashCode.Combine(X, Y, Z);
+    }
+
+    public record Point4D : PointR
+    {
+        public double CT { get; set; }
+    }
+}
---
+++ Records/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Records
+{
+    class Program
+    {
+        static void Main()
+        {
+            //string s1 = "Hello";
+            //string s2 = "Hello";
+
+            //Console.WriteLine(s1 == s2);
+
+            PointR r1 = new PointR { X = 1, Y = 2, Z = 3 };
+            PointR r2 = new PointR { X = 1, Y = 2, Z = 3 };
+
+            PointS s1 = new PointS { X = 1, Y = 2, Z = 3 };
+            PointS s2 = new PointS { X = 1, Y = 2, Z = 3 };
+
+            PointC c1 = new PointC { X = 1, Y = 2, Z = 3 };
+            PointC c2 = new PointC { X = 1, Y = 2, Z = 3 };
+
+            //IEquatable<PointR> eq = r1;
+
+            Console.WriteLine(r1 != r2);
+            //Console.WriteLine(c1 == c2);
+            //Console.WriteLine(s1 == s2);
+
+            PointR r3 = r1 with { X = 27, Z = 200 };
+
+            Console.WriteLine(object.ReferenceEquals(r3,r1));
+
+            Console.WriteLine(r1);
+            Console.WriteLine(r3);
+
+            PointR p4 = new Point4D { X = 1, Y = 2, Z = 3, CT = 4 };
+
+            Console.WriteLine(p4);
+
+            // Decomposition only provided automatically for positional record
+            //var (x, y, _) = r3;
+
+            //Console.WriteLine(x);
+            //Console.WriteLine(y);
+        }
+    }
+}
